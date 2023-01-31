7d5668796407c65b8d3595471332d409b8052ca5
vsndhra <81941104+vsndhra@users.noreply.github.com>
2023-01-31 07:30:22 +0530

    Add files via upload

---
+++ sjfs/Constants.java
@@ -0,0 +1,7 @@
+package com.sjfs;
+
+public class Constants {
+	public static final int NO_OF_TASKS = 30;
+	public static final int NO_OF_DATA_CENTERS = 5;
+	public static final int POPULATION_SIZE = 25;
+}
